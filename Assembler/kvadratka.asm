cc57b1bf6e97e094f030fe3dfbcb41dbd055f517
WhiteDolphin <whitedolphin@LAPTOP-IT71GF5N.localdomain>
2022-12-14 15:12:29 +0300

    :disc

--- Assembler/kvadratka.asm
+++ Assembler/kvadratka.asm
@@ -1,9 +1,12 @@
-push 5
+push 1
 pop ax
-push 2
+push 1
 pop bx
-call :lin_solve
-push cx
+push -6
+pop cx
+
+call :disc
+push dx
 out
 hlt
 
@@ -13,6 +16,21 @@ push bx
 push -1
 mul
 div
-pop cx
+pop ex
+ret
+
+:disc
+push bx
+push bx
+mul
+
+push 4
+push ax
+push cx
+mul
+mul
+
+sub
+pop dx
 ret
 
